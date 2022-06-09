1bfba59abe6161d581a456921bd77383b3f70566
Olivia Lind <eo.lind@gmail.com>
2022-06-09 16:10:31 -0500

    created choreRepository

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Roommates.Models;
+using Roommates.Repositories;
+using System;
 using System.Collections.Generic;
 
 namespace Roommates
@@ -11,6 +13,8 @@ namespace Roommates
 
         static void Main(string[] args)
         {
+            RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+
             bool runProgram = true;
             while (runProgram)
             {
@@ -19,13 +23,42 @@ namespace Roommates
                 switch (selection)
                 {
                     case ("Show all rooms"):
-                        // Do stuff
+                        List<Room> rooms = roomRepo.GetAll();
+                        foreach (Room r in rooms)
+                        {
+                            Console.WriteLine($"{r.Name} has an Id of {r.Id} and a max occupancy of {r.MaxOccupancy}");
+                        }
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                     case ("Search for room"):
-                        // Do stuff
+                        Console.Write("Room Id: ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Room room = roomRepo.GetById(id);
+
+                        Console.WriteLine($"{room.Id} - {room.Name} Max Occupancy({room.MaxOccupancy})");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                     case ("Add a room"):
-                        // Do stuff
+                        Console.Write("Room name: ");
+                        string name = Console.ReadLine();
+
+                        Console.Write("Max occupancy: ");
+                        int max = int.Parse(Console.ReadLine());
+
+                        Room roomToAdd = new Room()
+                        {
+                            Name = name,
+                            MaxOccupancy = max
+                        };
+
+                        roomRepo.Insert(roomToAdd);
+
+                        Console.WriteLine($"{roomToAdd.Name} has been added and assigned an Id of {roomToAdd.Id}");
+                        Console.Write("Press any key to continue");
+                        Console.ReadKey();
                         break;
                     case ("Exit"):
                         runProgram = false;
---
+++ Repositories/ChoreRepository.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
+
+namespace Roommates.Repositories
+{
+    // this interacts with chore data & uses BaseRepository's connection property via inheritance
+    public class ChoreRepository : BaseRepository
+    {
+        // passes connection string to BaseRepository during instantiation
+        public ChoreRepository(string connectionString) : base(connectionString) { }
+
+        // returns a single chore with the given id
+        public Chore GetbyId(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = Connection.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        Chore chore = null;
+
+                        if (reader.Read())
+                        {
+                            chore = new Chore
+                            {
+                                Id = id,
+                                Name = reader.GetString(reader.GetOrdinal("Name"))
+                            };
+                        }
+                        return chore;
+                    }
+                }
+            }
+        }
+
+        // add a new chore to the database
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Chore (Name)
+                        OUTPUT INSERTED.Id
+                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
+
+                    chore.Id = id;
+                }
+            }
+        }
+        // get all of the chores in the db (as a list)
+        public List<Chore> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Id, Name FROM Chore";
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        // list that holds the chores retrieved from db
+                        List<Chore> chores = new List<Chore>();
+
+                        // Read() will return true if there's more data to read
+                        while (reader.Read())
+                        {
+                            int idColumnPosition = reader.GetOrdinal("Id");
+
+                            int idValue = reader.GetInt32(idColumnPosition);
+
+                            int nameColumnPosition = reader.GetOrdinal("Name");
+                            string nameValue = reader.GetString(nameColumnPosition);
+
+                            // creates a new chore object with data from the db
+                            Chore chore = new Chore
+                            {
+                                Id = idValue,
+                                Name = nameValue
+                            };
+
+                            // add the chore object to the list
+                            chores.Add(chore);
+                        }
+                        // return the list of chores
+                        return chores;
+                    }
+                }
+            }
+        }
+    }
+}
