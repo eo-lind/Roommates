9fe521d1cd6539ce741400d3a251050057707585
Olivia Lind <eo.lind@gmail.com>
2022-06-13 11:48:18 -0500

    delete chore method added

--- Repositories/ChoreRepository.cs
+++ Repositories/ChoreRepository.cs
@@ -165,5 +165,19 @@ namespace Roommates.Repositories
             }
         }
 
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
     }
 }
